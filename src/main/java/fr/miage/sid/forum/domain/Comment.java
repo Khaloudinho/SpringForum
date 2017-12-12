fc507a2698d239497e4c0b9ed8f0dda3dbe24127
Thomas Schersach <toj315@gmail.com>
2017-12-12 23:36:10 +0100

    Added basic config, controller and entities

---
+++ src/main/java/fr/miage/sid/forum/domain/Comment.java
@@ -0,0 +1,20 @@
+package fr.miage.sid.forum.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+
+  @NotNull
+  private String content;
+
+}
