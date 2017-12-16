f9b52f1d070ff3da5c749b0cc933714332cdc575
Thomas Schersach <toj315@gmail.com>
2017-12-16 12:55:16 +0100

    Improved models to match requirements

--- src/main/java/fr/miage/sid/forum/domain/Project.java
+++ src/main/java/fr/miage/sid/forum/domain/Project.java
@@ -4,21 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @Entity
-public class Comment {
+public class Project {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-
-  @NotNull
-  private String content;
-
 }
