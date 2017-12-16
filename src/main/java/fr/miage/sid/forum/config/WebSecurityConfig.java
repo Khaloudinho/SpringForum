fd008a46d9f32533d8438e88938132df3233fcf5
Thomas Schersach <toj315@gmail.com>
2017-12-16 01:15:52 +0100

    Better security config, bug remaining in withUser ..

--- src/main/java/fr/miage/sid/forum/config/WebSecurityConfig.java
+++ src/main/java/fr/miage/sid/forum/config/WebSecurityConfig.java
@@ -2,8 +2,6 @@ package fr.miage.sid.forum.config;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,27 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final DataSource dataSource;
-
   @Autowired
-  public WebSecurityConfig(@Qualifier("dataSource") DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
-
-  // TODO
+  private DataSource dataSource;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.jdbcAuthentication()
         .dataSource(dataSource)
-        .usersByUsernameQuery(
-            "select username, password, enabled from users where username=?")
-        // TODO Check query
+        .usersByUsernameQuery("select username, password, enabled from person where username=?")
         .authoritiesByUsernameQuery(
             "SELECT\n"
                 + "  username,\n"
@@ -39,10 +27,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 + "FROM person\n"
                 + "  INNER JOIN person_roles prole ON person.id = prole.user_id\n"
                 + "  INNER JOIN role r ON prole.roles_id = r.id where username=?")
-        .passwordEncoder(new BCryptPasswordEncoder());
-    // TODO Try to male withUser works
-    // .withUser("");
+        .withUser("user").password("password").roles("USER");
   }
+
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
