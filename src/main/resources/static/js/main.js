7b9e5e9ea7ff7801ec9070c61e98017c53ed9e56
Thomas Schersach <toj315@gmail.com>
2018-01-21 23:53:55 +0100

    Flag is changing when switching lang

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,16 +9,28 @@
     headers: {[header]: token}
   })
 
-  const changers = document.querySelectorAll('.lang-changer')
+  const enChanger = document.querySelector('.en-lang-changer')
+  const frChanger = document.querySelector('.fr-lang-changer')
   const logoutBtn = document.getElementById('logoutBtn')
   const logoutForm = document.getElementById('logoutForm')
 
-  for (changer of changers) {
-    changer.addEventListener('click', function () {
-      const lang = this.dataset.lang
-      window.location.href = this.href + '?lang=' + lang
-    })
+  if (window.location.href.includes('?lang=en')) {
+    enChanger.style.display = 'none'
+  } else {
+    frChanger.style.display = 'none'
   }
+
+  enChanger && enChanger.addEventListener('click', function () {
+    window.location.href = this.href + '?lang=en'
+    enChanger.style.display = 'none'
+    frChanger.style.display = 'flex'
+  })
+
+  frChanger && frChanger.addEventListener('click', function () {
+    window.location.href = this.href + '?lang=fr'
+    frChanger.style.display = 'none'
+    enChanger.style.display = 'flex'
+  })
 
   if (logoutBtn) {
     logoutBtn.addEventListener('click', () => {
